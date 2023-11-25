bd9e92579ace69c01ff28c960db3267e8d0ac279
zhouzhao01 <caulks-rudders.0n@icloud.com>
2023-11-25 12:58:05 +0800

    CNN

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -101,40 +101,50 @@ class Classifier(nn.Module): # use softmax
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
-
+            
+            nn.Dropout(),
             nn.Conv2d(64, 128, 3, 1, 1), # [128, 64, 64]
             nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
 
+            nn.Dropout(),
             nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(2, 2, 0),      # [256, 16, 16]
 
+            nn.Dropout(),
             nn.Conv2d(256, 512, 3, 1, 1), # [512, 16, 16]
             nn.BatchNorm2d(512),
             nn.ReLU(),
             nn.MaxPool2d(2, 2, 0),       # [512, 8, 8]
             
+            nn.Dropout(),
             nn.Conv2d(512, 1024, 3, 1, 1), # [512, 8, 8]
             nn.BatchNorm2d(1024),
             nn.ReLU(),
             nn.MaxPool2d(2, 2, 0),       # [1024, 4, 4]
         )
         self.fc = nn.Sequential(
+
+            # nn.Dropout(),
             nn.Linear(1024*4*4, 1024*4),
             nn.ReLU(),
 
+            # nn.Dropout(),
             nn.Linear(1024*4, 1024),
             nn.ReLU(),
 
+            # nn.Dropout(),
             nn.Linear(1024, 512),
             nn.ReLU(),
-
+            
+            # nn.Dropout(),
             nn.Linear(512, 256),
             nn.ReLU(),
 
+            # nn.Dropout(),
             nn.Linear(256,11),
             nn.ReLU(),
         )
